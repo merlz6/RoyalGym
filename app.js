e60b6e22c68382b34ac0b889e993788502c8acde
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2020-03-04 18:50:01 -0500

    added some css touchs

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("logged")
+
+function getEmails(){
+  let firstName = document.getElementById("firstNameInput").value
+  let lastName = document.getElementById("lastNameInput").value
+  let email = document.getElementById("lastNameInput").value
+
+  console.log(firstName + " " + lastName + " email is : " + email)
+}
